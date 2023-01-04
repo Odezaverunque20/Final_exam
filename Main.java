04bd6a1e2d69928a6c709354268a527900e687c0
Odeza M. Verunque <116275902+Odezaverunque20@users.noreply.github.com>
2023-01-04 15:12:52 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,18 @@
 public class Main
 {
+  public static void main(String[]orgs){
+		Customer Customer;
+		RegularCustomer RegularCustomer = new RegularCustomer("Dezzy", 1000);
+		SeniorCustomer  SeniorCustomer = new SeniorCustomer("Rexxy", 1000);
+
+		Customer = RegularCustomer;
+		
+		
+		System.out.println(Customer.calculateBill() + "\t" + Customer.getName() + ".");
+
+		Customer = SeniorCustomer;
+		
+		
+		System.out.println(Customer.calculateBill() + "\t" + Customer.getName() + ".");
+    }
 }
